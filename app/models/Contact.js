e1a2cf8873a4f956f5763491346eee3a185fce7f
Alex Ehrnschwender <alex.ehrnschwender@dena.com>
2013-12-15 17:05:20 -0800

    patch and update methods for resources in mirror client

--- app/models/Contact.js
+++ app/models/Contact.js
@@ -3,5 +3,13 @@
 module.exports = {
     id: '',
     displayName: '',
-    imageUrls: []
+    imageUrls: [],
+    source: '',
+    type: '',
+    acceptTypes: [],
+    phoneNumber: '',
+    priority: 0,
+    acceptCommands: [],
+    speakableName: '',
+    sharingFeatures: []
 };
